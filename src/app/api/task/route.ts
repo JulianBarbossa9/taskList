72ff8ce6b93a22c1da11f31963acdc84a8dd4b7f
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-02 03:14:30 -0500

    add endpoints v1

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server"
+// import prisma from "../../../../lib/prisma"
+import { NextApiRequest, NextApiResponse } from "next"
 
 export async function GET(request:Request, context: any ) {
   console.log(context.params)
@@ -8,3 +10,29 @@ export async function GET(request:Request, context: any ) {
     status: 200
   })
 }
+
+export async function POST(request: Request, context: any){
+  console.log(context.params)
+  return NextResponse.json({
+    message: 'Hello from POST all task',
+  },{
+    status: 200
+  })
+}
+
+
+// export async function POST(request:NextApiRequest, response: NextApiResponse) {
+//   const { title, description } = request.body
+  
+//   try {
+//     await prisma?.task.create({
+//       data: {
+//         title,
+//         description
+//       }
+//     })
+//     response.status(200).json({message: 'Task created'})
+//   } catch (error) {
+//     console.log('Bad Request')
+//   }
+// }
