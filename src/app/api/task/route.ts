d85162ea0be315668ffa9f8c4583485e46adab19
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-03 23:18:52 -0500

    add endpoints GET and post with prisma

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,38 +1,32 @@
 import { NextResponse } from "next/server"
 // import prisma from "../../../../lib/prisma"
 import { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "../../../../lib/prisma"
 
-export async function GET(request:Request, context: any ) {
-  console.log(context.params)
-  return NextResponse.json({
-    message: 'Hello from Get all task',
-  },{
-    status: 200
-  })
+export async function GET(request:Request ) {
+ const task = await prisma.task.findMany()
+ return NextResponse.json(task)
 }
 
-export async function POST(request: Request, context: any){
-  console.log(context.params)
-  return NextResponse.json({
-    message: 'Hello from POST all task',
-  },{
-    status: 200
-  })
+export async function POST(request:Request) {
+  try {
+    const json = await request.json()
+
+    const task = await prisma.task.create({
+      data: json
+    })
+
+    return new NextResponse(JSON.stringify(task), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error: any){
+    if(error.code === "P2002"){
+      return new NextResponse("This task already exist", {
+        status: 409,
+      })
+    }
+    return new NextResponse(error.message, { status: 500  })
+  }
 }
 
-
-// export async function POST(request:NextApiRequest, response: NextApiResponse) {
-//   const { title, description } = request.body
-  
-//   try {
-//     await prisma?.task.create({
-//       data: {
-//         title,
-//         description
-//       }
-//     })
-//     response.status(200).json({message: 'Task created'})
-//   } catch (error) {
-//     console.log('Bad Request')
-//   }
-// }
