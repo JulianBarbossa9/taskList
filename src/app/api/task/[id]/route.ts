b1ac923896aaccc3a9a2a1e17b342f692cab54a6
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-03 23:19:23 -0500

    add endpoints for dynamcis routes

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -1,34 +1,60 @@
 
 import { NextResponse } from "next/server";
+import { prisma } from "../../../../../lib/prisma";
 
-export async function GET(request:Request, context: any ) {
+export async function GET(
+  request:Request, 
+  { params } : { params: {id : string }}) {
   
-  console.log(context.params)
-  
-  return NextResponse.json({
-    message: 'Hello from Get unique task',
-    id: context.params
-  },{
-    status: 200
-  })
+    const id = params.id
+    const task = await prisma.task.findUnique({
+      where: {
+        id,
+      }
+    })
+    if (!task){
+      return new NextResponse("The task with that id doesn't exist", { status: 404})
+    }
+    return NextResponse.json(task)
 }
 
-//uptade unique task
-export async function PUT(request:Request, context: any ) {
-  console.log(context.params)
-  return NextResponse.json({
-    message: 'Update task PUT',
-  },{
-    status: 200
-  })
+
+export async function PATCH(
+  request:Request, 
+  { params } : { params: { id: string}}) {
+  
+    const id = params.id
+    let json = await request.json()
+
+    const updateTask = await prisma.task.update({
+      where: {id},
+      data: json,
+    })
+
+    if(!updateTask) {
+      return new NextResponse("The task with that id doesn't exist", { status: 404})
+    }
+
+    return NextResponse.json(updateTask, { status: 200})
 }
 
-export async function DELETE(request:Request, context: any ) {
-  console.log(context.params)
-  return NextResponse.json({
-    message: 'Delete unique task ',
-  },{
-    status: 200
-  })
+export async function DELETE(
+  request:Request, 
+  { params } : { params: {id :string}}) {
+  
+    console.log(params)
+    try {
+      const id = params.id
+      await prisma.task.delete({
+        where: { id }
+      })
+      return new NextResponse(null, { status: 204 })
+      
+    } catch (error: any) {
+      if (error.code === "P2025"){
+        return new NextResponse("The task with that id doesn't exist", { status: 404})
+      }
+      return new NextResponse(error.message, { status: 500 })
+    }
 }
 
